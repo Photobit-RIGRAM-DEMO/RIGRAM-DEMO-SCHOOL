a7efd55cdd122559ea98598274750b1e0aa793a6
YunHa Song <yunhaphoto12@gmail.com>
2026-06-29 18:45:07 +0900

    중간 배포5

--- src/app/student/[schoolId]/layout.tsx
+++ src/app/student/[schoolId]/layout.tsx
@@ -24,7 +24,7 @@ export default async function SchoolLayout({
     school = data;
   }
 
-  let mainClass =
+  const mainClass =
     'flex justify-center items-start flex-1 p-5 md:px-10 md:pb-0';
 
   return (
